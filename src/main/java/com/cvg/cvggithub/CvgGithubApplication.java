171a13f3c802a773e2ff08cfe65c55b4dcc4749e
Cristhian Villegas <cristianvg9692@gmail.com>
2023-01-26 23:10:01 -0600

    Fisrt version

---
+++ src/main/java/com/cvg/cvggithub/CvgGithubApplication.java
@@ -0,0 +1,13 @@
+package com.cvg.cvggithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvgGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvgGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cvg/cvggithub/CvgGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvg.cvggithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvgGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
